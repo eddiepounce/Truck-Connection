d71ee04be84caec92013bb0be4d94fac087eade4
eddiepounce <84339861+eddiepounce@users.noreply.github.com>
2021-05-17 19:21:30 +0100

    License added
    Added License to repository and file

--- PPMCreation/PPMCreation.ino
+++ PPMCreation/PPMCreation.ino
@@ -1,5 +1,6 @@
 /*
-Make a PPM stream from PWM's and Lighting on truck.
+Make a PPM stream from PWM's and Lighting connections on truck.
+Copyright (C) 2021  Eddie Pounce
 
 	Channel
 		1		Proportional	5th Wheel Locking - to drive trailer legs (PWM from servo)
@@ -21,6 +22,23 @@ void interruptReadChannels() {
 
 setup	
 enableInterrupt(inputPin, interruptReadChannels, PULSE_EDGE);
+
+    Copyright
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+	84339861+eddiepounce@users.noreply.github.com
 
 */
 
